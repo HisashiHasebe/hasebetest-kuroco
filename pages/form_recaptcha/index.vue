e7ec017b355108ee9cb68c8a297123275c428381
HisashiHasebe <hhasebe@diverta.co.jp>
2023-07-07 15:45:21 +0800

    update

--- pages/form_recaptcha/index.vue
+++ pages/form_recaptcha/index.vue
@@ -1,239 +1,72 @@
 <template>
   <div>
-    <h1>FORMページ</h1>
-
-    <form v-if="!submitted" ref="form">
-      <div v-if="error" class="error">
-        <p v-for="(err, idx) in error" :key="idx">
-          {{ err }}
-        </p>
-      </div>
-
-      <div class="row--status">
-        <h2>フォーム名</h2>
-        <div>{{ name }}</div>
-      </div>
-
-      <div class="row--status">
-        <h2>説明</h2>
-        <div>
-          <p v-for="(line, idx) in textLines2texts(info)" :key="idx">
-            {{ line }}
+    <section>
+      <h1>Contact
+      </h1>
+      <form>
+        <div v-if="error" class="error">
+          <p v-for="(err, idx) in error" :key="idx">
+            {{ err }}
           </p>
         </div>
-      </div>
 
-      <div class="row--status">
-        <h2>サンクス文言</h2>
+        <div v-if="submitted">Inquiry submitted.</div>
+
         <div>
-          <p v-for="(line, idx) in textLines2texts(thanksText)" :key="idx">
-            {{ line }}
-          </p>
+          <dl>
+            <dt>Name</dt>
+            <dd><input v-model="submitData.name" :name=name type="text"></dd>
+          </dl>
+          <dl>
+            <dt>Email</dt>
+            <dd><input v-model="submitData.from_mail" :name=from_mail type="text"></dd>
+          </dl>
+          <dl>
+            <dt>Messege</dt>
+            <dd><textarea v-model="submitData.body" :name=body></textarea></dd>
+          </dl>
         </div>
-      </div>
 
-      <div class="row--status">
-        <h2>フォーム項目</h2>
-        <div class="row--internal">
-          <div v-for="col in cols" :key="col.key">
-            <p>[{{ col.title }}]</p>
-            <pre>{{ col }}</pre>
-          </div>
-        </div>
-      </div>
-
-      <div v-for="col in cols" :key="col.objKey" class="form-group">
-        <h2>[{{ col.title }}]</h2>
-        <input
-          v-if="col.title === 'file'"
-          :name="col.objKey"
-          type="file"
-          @change="uploadFile"
-        />
-        <input v-else :name="col.objKey" type="text" />
-      </div>
-
-      <div class="row--bottom-next">
         <inquiry-recaptcha ref="recaptcha" :is-succeeded.sync="canSubmit" />
-        <div>確認：{{canSubmit}}</div>
-        <button class="btn btn-primary btn-lg" @click="handleOnSubmit">
-          submit
-        </button>
-      </div>
-    </form>
-
-    <form v-else>
-      <div class="row--status">
-        <h2>問い合わせID</h2>
-        <div>
-          {{ submittedId }}
-        </div>
-      </div>
-
-      <div class="row--status">
-        <h2>サンクス文言</h2>
-        <div>
-          <p v-for="(line, idx) in textLines2texts(thanksText)" :key="idx">
-            {{ line }}
-          </p>
-        </div>
-      </div>
-
-      <div class="row--bottom-back">
-        <button @click="handleOnBack">back</button>
-      </div>
-    </form>
+        <button @click.prevent="handleOnSubmit" :disabled="!canSubmit">Submit</button>
+      </form>
+    </section>
   </div>
 </template>
 
 <script>
-const FORM_ID = 6 // 作成したフォーム定義のID
 import InquiryRecaptcha from '@/components/inquiry/inquiry-recaptcha.vue'
 
 export default {
   components: {
     InquiryRecaptcha,
   },
-  async asyncData({ $axios }) {
-    const response = await $axios.$get(
-      `/rcms-api/1/form/${FORM_ID}`
-    )
-    return {
-      name: response.details.inquiry_name,
-      info: response.details.inquiry_info,
-      thanksText: response.details.thanks_text,
-      cols: Object.entries(response.details.cols).map(([k, v]) => ({
-        objKey: k,
-        ...v,
-      })),
-    }
-  },
-  data: () => {
+  data() {
     return {
       submitted: false,
-      submittedId: null,
+      submitData: {},
       error: null,
-      file_id: null,
       canSubmit: false,
     }
   },
-  methods: {
-    async uploadFile(e) {
-      const fm = new FormData();
-      fm.append('file', e.target.files[0]);
-
-      const { file_id } = await this.$axios.$post(`/rcms-api/22/file`, fm, {
-        headers: {
-          'Content-Type': 'multipart/form-data', // required to post file as a binary
-        },
-      });
-      this.file_id = file_id;
-    },
-
-    textLines2texts(textLines = '') {
-      return textLines.split('\r\n')
-    },
-    async handleOnSubmit(e) {
-      e.preventDefault()
-
-      // collect input elements
-      const formInputElements = Array.from(this.$refs.form.elements).filter(
-        (elm) => elm.tagName.toLowerCase() === 'input'
-      )
-
-      // transform key:value inputs to an object
-      const body = formInputElements
-        .map((elm) => ({ [elm.name]: elm.value }))
-        .reduce((prev, cur) => ({ ...prev, ...cur }), {})
-
-        // apply file_id instead of the actual file input value
-      if (body.ext_03) {
-        body.ext_03 = {
-          file_id: this.file_id,
-        };
-      }
-
-      try {
-        // post data
-        const { id } = await this.$axios.$post(
-          `/rcms-api/1/form?id=${FORM_ID}`,
-          body
-        )
-        this.error = null
-        this.submittedId = id
-        this.submitted = true
-      } catch (e) {
-        this.error = [`${e}`, ...e.response.data.errors]
-      }
-    },
-    handleOnBack(e) {
-      e.preventDefault()
-      this.submitted = false
-    },
-    async submit() {
-      this.$axios.$post('/form', {
-        ...this.formData,
-        recaptcha_response: await this.$refs.recaptcha.fetchResponse(),
-      }).then((response) => {
-        if (response.errors.length > 0) {
-          throw new Error('Post failed.');
-        }
-        this.$router.push({ path: '/form/' });
-      }).catch((response) => {
-        this.canSubmit = false;
-      });
-      await this.$recaptcha.reset();
-    },
+  async asyncData({ $axios }) {
+    return {
+      response: await $axios.$get('/rcms-api/1/form/12'),
+    };
   },
-}
+  methods: {
+    async handleOnSubmit() {
+      this.submitData.recaptcha_response = await this.$refs.recaptcha.fetchResponse()
+      try {
+        await this.$axios.$post('/rcms-api/1/form?id=12',{ ...this.submitData })
+        this.submitted = true
+        this.error = null
+      } catch (e) {
+        console.error(e)
+        this.error = e.response.data.errors
+      }
+      await this.$recaptcha.reset();
+    }
+  }
+};
 </script>
-
-<style scoped>
-.error {
-  color: red;
-}
-.error > *:first-child {
-  font-weight: bold;
-}
-
-.row--status {
-  display: flex;
-  border-top: 1px solid black;
-}
-.row--status > *:first-child {
-  background-color: yellow;
-  min-width: 15rem;
-  max-width: 15rem;
-  border-right: 1px solid black;
-}
-
-.row--form {
-  display: flex;
-  border-top: 1px solid black;
-}
-.row--form > *:first-child {
-  background-color: aquamarine;
-  min-width: 15rem;
-  max-width: 15rem;
-  border-right: 1px solid black;
-}
-
-.row--bottom-next {
-  padding: 8px 16px;
-  display: flex;
-  justify-content: flex-end;
-}
-.row--bottom-back {
-  padding: 8px 16px;
-  display: flex;
-  justify-content: flex-start;
-}
-
-.row--internal {
-  display: flex;
-}
-
-form > *:nth-last-child(2) {
-  border-bottom: 1px solid black;
-}
-</style>
