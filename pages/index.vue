7cf007c4a6ea116da19fd7fea60f8f8d9075922e
HisashiHasebe <hhasebe@diverta.co.jp>
2023-07-13 12:47:47 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,7 @@
       <li><nuxt-link to="/text-to-speech/">google text to speechのお試し(base64エンコードされたmp3ファイルのデータをデコードして表示するところまで実装済み)</nuxt-link></li>
       <li><nuxt-link to="/form_recaptcha/">reCAPTCHA対応フォーム</nuxt-link></li>
       <li><nuxt-link to="/recaptha_for_password_reminders/">reCAPTCHA対応パスワードリマインダー</nuxt-link></li>
+      <li><nuxt-link to="/auto_contents_for_edge/">OpenAIがユーザー毎に最適なコンテンツを表示する</nuxt-link></li>
     </ul>
   </div>
 </template>
