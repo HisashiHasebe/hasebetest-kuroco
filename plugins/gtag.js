1fbae0998cbbcbf1c6896ad8530c6c0fa2b473d4
HisashiHasebe <hhasebe@diverta.co.jp>
2023-07-27 13:27:09 +0800

    ga4

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -3,7 +3,7 @@ import VueGtag from 'vue-gtag';
 Vue.use(VueGtag, {
     config: {
         id: 'G-T39XGR8R0Y',
-        additionalParams: {
+        custom_map: {
             page_module: 'page_module',
             main_module_id: 'main_module_id',
             page_sysnm: 'page_sysnm',
