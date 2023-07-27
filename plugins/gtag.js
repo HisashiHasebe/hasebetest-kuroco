d6388c0d36ca7cd19bf20de8f399bfafcb25e7e4
HisashiHasebe <hhasebe@diverta.co.jp>
2023-07-27 13:21:10 +0800

    ga4

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,5 +1,13 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
 Vue.use(VueGtag, {
-    config: { id: 'G-T39XGR8R0Y' }
+    config: {
+        id: 'G-T39XGR8R0Y',
+        additionalParams: {
+            page_module: 'page_module',
+            main_module_id: 'main_module_id',
+            page_sysnm: 'page_sysnm',
+            group_id: 'group_id',
+        }
+      }
 });
