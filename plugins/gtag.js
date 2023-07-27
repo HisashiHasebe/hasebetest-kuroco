b8d23678557a0f77b31402831e121cfa301539a6
HisashiHasebe <hhasebe@diverta.co.jp>
2023-07-27 14:00:31 +0800

    test

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -3,13 +3,13 @@ import VueGtag from 'vue-gtag';
 Vue.use(VueGtag, {
     config: {
         id: 'G-T39XGR8R0Y',
-        custom_map: {
-            page_module: 'page_module',
-            main_module_id: 'main_module_id',
-            page_sysnm: 'page_sysnm',
-            group_id: 'group_id',
-        }
       },
+    custom_map: {
+        page_module: 'page_module',
+        main_module_id: 'main_module_id',
+        page_sysnm: 'page_sysnm',
+        group_id: 'group_id',
+    },
     event:{
         any_event_name: {
             page_module: 'page_module',
