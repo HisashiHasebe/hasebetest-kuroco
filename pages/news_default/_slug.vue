e121b67f02865feba0ad6fb4ab59687007a7f29f
HisashiHasebe <hhasebe@diverta.co.jp>
2023-08-02 10:32:59 +0800

    update

--- pages/news_default/_slug.vue
+++ pages/news_default/_slug.vue
@@ -12,5 +12,13 @@
         response: await $axios.$get(`/rcms-api/1/newsdetail/${params.slug}`),
       };
     },
+    mounted() {
+    if (process.browser) {
+      const slug = this.$route.params.slug // ページのURLなどからslugを取得(実際の構成に合わせて変更して下さい)
+      this.$gtag('event', 'page_view', {
+        'slug': slug
+      })
+    }
+  }
   };
   </script>
