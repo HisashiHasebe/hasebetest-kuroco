38475d4cbfb5035bc8054db32672cf21de2b4b2a
HisashiHasebe <hhasebe@diverta.co.jp>
2023-08-08 12:53:40 +0800

    add:フォームのバッチ送信

---
+++ pages/form_batch_sending/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <section>
+      <h1>Contact
+      </h1>
+      <form>
+        <div v-if="error" class="error">
+          <p v-for="(err, idx) in error" :key="idx">
+            {{ err }}
+          </p>
+        </div>
+
+        <div v-if="submitted">Inquiry submitted.</div>
+
+        <div>
+          <dl>
+            <dt>Name</dt>
+            <dd><input v-model="submitData.name" :name=name type="text"></dd>
+          </dl>
+          <dl>
+            <dt>Email</dt>
+            <dd><input v-model="submitData.from_mail" :name=from_mail type="text"></dd>
+          </dl>
+          <dl>
+            <dt>Messege</dt>
+            <dd><textarea v-model="submitData.body" :name=body></textarea></dd>
+          </dl>
+          <dl>
+            <dt>Category</dt>
+            <dd>
+              <ul>
+                <li>
+                  <label>
+                    <input type="checkbox" id="10" value="10" v-model="submitData.category" />
+                    About Kuroco
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input type="checkbox" id="11" value="11" v-model="submitData.category" />
+                    About KurocoEdge
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input type="checkbox" id="12" value="12" v-model="submitData.category" />
+                    About RCMS
+                  </label>
+                </li>
+              </ul>
+            </dd>
+          </dl>
+        </div>
+        <button @click.prevent="handleOnSubmit">Submit</button>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      submitted: false,
+      submitData: {
+        category: [],
+      },
+      error: null,
+    }
+  },
+  methods: {
+    async handleOnSubmit() {
+      // Create payload
+      const payload = {
+        list: this.submitData.category.map(categoryId => ({
+          name: this.submitData.name,
+          email: this.submitData.from_mail,
+          body: this.submitData.body,
+          inquiry_category_id: parseInt(categoryId), // Convert category ID to integer
+        })),
+      };
+      //Post processing to Kuroco endpoints
+      try {
+        await this.$axios.$post('/rcms-api/1/message/bulk_upsert', payload);
+        this.submitted = true;
+        this.error = null;
+      } catch (e) {
+        this.error = e.response.data.errors;
+      }
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,7 @@
       <li><nuxt-link to="/auto_contents_for_edge/">OpenAIがユーザー毎に最適なコンテンツを表示する(try中)</nuxt-link></li>
       <li><nuxt-link to="/assets/">assets画像の表示</nuxt-link></li>
       <li><nuxt-link to="/markdown_content/">markdown_content入力コンテンツの表示</nuxt-link></li>
+      <li><nuxt-link to="/form_batch_sending/">複数回答の同時送信フォーム</nuxt-link></li>
     </ul>
   </div>
 </template>
