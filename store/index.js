282f95a6040d6ce6ad4c6b38fe708e2ea8a0c4ac
HisashiHasebe <hhasebe@diverta.co.jp>
2024-01-03 16:11:00 +0800

    storeのバグ修正

--- store/index.js
+++ store/index.js
@@ -28,7 +28,7 @@ export const actions = {
     await this.$axios.$post('/rcms-api/18/login', payload)
 
     const profileRes = await this.$axios.$get('/rcms-api/18/profile')
-    commit('setProfile', { profile: profileRes.data })
+    commit('setProfile', { profile: profileRes })
     commit('updateLocalStorage', { authenticated: true })
   },
   async logout({ commit }) {
@@ -52,13 +52,14 @@ export const actions = {
     }
     try {
       const profileRes = await this.$axios.$get('/rcms-api/18/profile')
-      commit('setProfile', { profile: profileRes.data })
+      console.log('Profile data from API:', profileRes);
+      commit('setProfile', { profile: profileRes })
     } catch {
       await dispatch('logout')
       throw new Error('need to login')
     }
 
     const profileRes = await this.$axios.$get('/rcms-api/18/profile')
-    commit('setProfile', { profile: profileRes.data })
+    commit('setProfile', { profile: profileRes })
   }
 }
