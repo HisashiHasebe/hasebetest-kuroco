ec67dd11a3b72e80a7d3c600461c2ae1f7918d67
HisashiHasebe <hhasebe@diverta.co.jp>
2024-02-07 11:15:05 +0800

    update:vue-pdf

---
+++ pages/vue-pdf/demo3.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<pdf
+			src="https://hasebetest.g.kuroco-img.app/files/user/test_file.pdf"
+			@num-pages="pageCount = $event"
+			@page-loaded="currentPage = $event"
+		></pdf>
+	</div>
+</template>
+
+<script>
+
+import pdf from 'vue-pdf'
+
+export default {
+	components: {
+		pdf
+	},
+}
+
+</script>
