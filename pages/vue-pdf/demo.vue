a1d33fdebdba2eb47f2e9d03455d80f6502f1922
HisashiHasebe <hhasebe@diverta.co.jp>
2024-02-07 10:57:17 +0800

    add:vue-pdf

---
+++ pages/vue-pdf/demo.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		{{currentPage}} / {{pageCount}}
+		<pdf
+			src="https://hasebetest.g.kuroco-front.app/pdfjs-4.0.379-dist/web/compressed.tracemonkey-pldi-09.pdf"
+			@num-pages="pageCount = $event"
+			@page-loaded="currentPage = $event"
+		></pdf>
+	</div>
+</template>
+
+<script>
+
+import pdf from 'vue-pdf'
+
+export default {
+	components: {
+		pdf
+	},
+	data() {
+		return {
+			currentPage: 0,
+			pageCount: 0,
+		}
+	}
+}
+
+</script>
